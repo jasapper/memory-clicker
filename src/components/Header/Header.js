54fa81cf6e0392695f809141faa8609ddaa2eb11
jasapper <jasap98@yahoo.com>
2018-04-25 00:57:07 -0400

    Components completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Memory Clicker!</h1>
+    <h2>
+      Click on an image to earn points, but don't click the same image/character more than once!
+    </h2>
+  </header>
+);
+
+export default Header;
